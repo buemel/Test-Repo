894cba4c227be029b83685319fe152ee7fda6ae1
Thomas Markert <thomas_markert@yahoo.de>
2020-05-09 13:39:21 +0200

    changes

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-import os
+import sys
+import sympy
 
-
-cwd = os.getcwd()
-print(cwd)
+n = 0
+isprime = False
+while True:
+    n += 1
+    isprime = sympy.isprime(n*n + n + 41)
+    if not isprime:
+        print("for n = %d NOT PRIME" % (n))
+        break
+    if n == 1000:
+        print("reached %d" % (n))
+        break
