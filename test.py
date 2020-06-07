f7f4161efc8d361b1fb8523ba35325a8cb700f74
Thomas Markert <thomas_markert@yahoo.de>
2020-06-07 18:00:10 +0200

    changed

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
-import sys
-import sympy
-
-n = 0
-isprime = False
-while True:
-    n += 1
-    isprime = sympy.isprime(n*n + n + 41)
-    if not isprime:
-        print("for n = %d NOT PRIME" % (n))
-        break
-    if n == 1000:
-        print("reached %d" % (n))
-        break
+# Syntax: Python 3.7
+n = int(input('Fakultät von n = '))
+f = 1
+for i in range(1, n + 1):
+    f *= i
+print(f'{n}! = {f}')
